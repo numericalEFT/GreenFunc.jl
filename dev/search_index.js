cef8402a939f997868170eec0d249ba2bd8264e8
Documenter.jl <documenter@juliadocs.github.io>
2021-11-08 04:13:39 +0000

    build based on cc34165

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GreenFunc","category":"page"},{"location":"#GreenFunc","page":"Home","title":"GreenFunc","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GreenFunc.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GreenFunc]","category":"page"}]
+}
