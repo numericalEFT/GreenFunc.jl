01cc51f4c88082db0b43f2ee0a9a49dcdd4b59a1
Tao Wang <fsxbhyy@gmail.com>
2021-12-20 15:28:15 -0500

    add Lehmann dependence

--- src/green/Green.jl
+++ src/green/Green.jl
@@ -7,28 +7,38 @@ General Green's function.
 module GreenBasic
 
 export Green2
-using StaticArrays, GreenFunc
+using StaticArrays, GreenFunc, Lehmann
 
 """
 Green's function with two external legs. The structure saves a function G( τ, q, σ)
 and corresponding grids of τ, q and σ.
 
 #Members:
-- 'timeType': Whether the Green's function is in time space or frequency space.
-- 'spaceType': Whether the Green's function is in coordinate space or momentum space.
+- 'timeType': Whether the Green's function is in time/frequency/dlr space
+- 'spaceType': Whether the Green's function is in coordinate space or momentum space
 - 'color': Indices of species of Green's function (such as different spin values)
+- 'isFermi': Particle is fermi or boson
+- 'timeSymmetry': Whether the Green's function has particle-hole symmetry, anti-particle-hole symmetry or none of them
+- 'spaceSymmetry': Symmetry of lattice
 - 'timeGrid': Time or Frequency grid
+- 'β':Inverse temperature
 - 'spaceGrid': Coordinate or momentum grid
-- 'value': Data of the Green's fucntion G( τ, q, σ)
+- 'instant': Instantaneous part of Green's function that is δ(τ) in τ space.
+- 'dynamic': Dynamic part of Green's function
 """
+
 mutable struct Green2{T<:AbstractFloat,TGT,SGT,CT}
     timeType::Symbol
     spaceType::Symbol
-    particleType::Symbol
+    isFermi::Bool
+    timeSymmetry::Symbol
+    spaceSymmetry::Any
+    β::Float64
     color::CT
     timeGrid::TGT
     spaceGrid::SGT
-    value::AbstractArray{T,3}
+    instant::AbstractArray{T,2}
+    dynamic::AbstractArray{T,4}
 
 
 
@@ -38,23 +48,86 @@ mutable struct Green2{T<:AbstractFloat,TGT,SGT,CT}
     create two-leg Green's function on tgrid, sgrid and color_n.
     The value of Green's function is initialized with zero.
     """
-    function Green2{T}(ttype,stype,ptype,color_n::CT,tgrid::TGT,sgrid::SGT)where{T<:AbstractFloat,TGT,SGT,CT}
-        val = zeros(T, (length(tgrid), length(sgrid),length(color_n)))
+    function Green2{T}(ttype,stype,isfermi,tsym, ssym, beta, color_n::CT,tgrid::TGT,sgrid::SGT)where{T<:AbstractFloat,TGT,SGT,CT}
+        static_val = zeros(T, (length(color_n),length(sgrid)))        
+        dynamic_val = zeros(T, (length(color_n), length(color_n), length(sgrid),length(tgrid)))
         timeType = ttype
         spaceType = stype
-        particleType = ptype
+        isFermi = isfermi
+        timeSymmetry = tsym
+        spaceSymmetry = ssym 
+        β = beta
         color = color_n
         timeGrid = tgrid
         spaceGrid = sgrid
-        value = val
-        return new{T,TGT,SGT,CT}(timeType,spaceType,particleType,color,timeGrid,spaceGrid,value)
+        instant = static_val
+        dynamic = dynamic_val
+        return new{T,TGT,SGT,CT}(timeType,spaceType,isFermi,timeSymmetry,spaceSymmetry,β,color,timeGrid,spaceGrid,instant,dynamic)
+    end 
+end
+
+"""
+Green's function with two external legs. The structure saves a function G( τ, q, σ)
+and corresponding grids of τ, q and σ.
+
+#Members:
+- 'timeType': Whether the Green's function is in time/frequency/dlr space
+- 'spaceType': Whether the Green's function is in coordinate space or momentum space
+- 'color': Indices of species of Green's function (such as different spin values)
+- 'isFermi': Particle is fermi or boson
+- 'timeSymmetry': Whether the Green's function has particle-hole symmetry, anti-particle-hole symmetry or none of them
+- 'spaceSymmetry': Symmetry of lattice
+- 'timeGrid': Time or Frequency grid
+- 'β':Inverse temperature
+- 'spaceGrid': Coordinate or momentum grid
+- 'instant': Instantaneous part of Green's function that is δ(τ) in τ space.
+- 'dynamic': Dynamic part of Green's function
+"""
+
+mutable struct Green2DLR{T<:AbstractFloat,TGT,SGT,CT}
+    timeType::Symbol
+    spaceType::Symbol
+    isFermi::Bool
+    timeSymmetry::Symbol
+    spaceSymmetry::Any
+    β::Float64
+    rtol::Float64
+    color::CT
+    timeGrid::TGT
+    spaceGrid::SGT
+    instant::AbstractArray{T,2}
+    dynamic::AbstractArray{T,4}
+    dlrGrid::DLRGrid
+
+
+    """
+         function GreenTwo{T,TGT,SGT,CT}(ttype,stype,color_n,tgrid,sgrid)where{T<:AbstractFloat,TGT,SGT,CT}
+
+    create two-leg Green's function on tgrid, sgrid and color_n.
+    The value of Green's function is initialized with zero.
+    """
+    function Green2DLR{T}(ttype,stype,isfermi,tsym, ssym, beta, color_n::CT,tgrid::TGT,sgrid::SGT)where{T<:AbstractFloat,TGT,SGT,CT}
+        static_val = zeros(T, (length(color_n),length(sgrid)))        
+        dynamic_val = zeros(T, (length(color_n), length(color_n), length(sgrid),length(tgrid)))
+        timeType = ttype
+        spaceType = stype
+        isFermi = isfermi
+        timeSymmetry = tsym
+        spaceSymmetry = ssym 
+        β = beta
+        color = color_n
+        timeGrid = tgrid
+        spaceGrid = sgrid
+        instant = static_val
+        dynamic = dynamic_val
+        return new{T,TGT,SGT,CT}(timeType,spaceType,isFermi,timeSymmetry,spaceSymmetry,β,color,timeGrid,spaceGrid,instant,dynamic)
     end 
 end
 
 
-Base.getindex(green::Green2, i ,j, k) = green.value[i,j,k]
-Base.getindex(green::Green2, i) = green.value[i]
-#Base.lastindex(green::GreenUR) = grid.size
+
+
+
 
 
 end
--- test/test_Green.jl
+++ test/test_Green.jl
@@ -3,11 +3,9 @@
         tgrid = [0.0,1.0]
         sgrid = [0.0,1.0]
         color_n = [0.0,1.0]
-	      green_simple = GreenBasic.Green2{Float64}(:freq,:mom,:fermi,color_n,tgrid,sgrid)
-        println(green_simple.value,green_simple[1,1,1])
-        green_simple.value[1,1,1] = 1.0
-        green_simple.timeType=:time
-        println(green_simple.timeType,green_simple[1,1,1])        
+        beta = 20.0
+	      green_simple = GreenBasic.Green2{Float64}(:freq,:mom,true,:particlehole,:none,beta,color_n,tgrid,sgrid)
+        println(green_simple.dynamic)        
     end
 end
 
