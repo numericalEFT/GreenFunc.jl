70b0719a7ecdd960846b2b9da8f76290c19e26f3
Peter <peter@DESKTOP-OQVHHLA.localdomain>
2022-09-19 21:26:41 +0800

    update MeshProduct

--- src/green/MeshProduct.jl
+++ src/green/MeshProduct.jl
@@ -20,42 +20,58 @@ end
 
 #TODO:return the sum of length of all meshes
 function Base.length(obj::MeshProduct)
-    return 1
+    len = 1
+    for l in length.(obj.mlist)
+        len = len * l
+    end
+    return len
 end
 
 #TODO:return the size of Ith mesh in mlist 
 function Base.size(obj::MeshProduct, I::Int)
-    return 1
+    return length.(obj.mlist)[I]
 end
 
 #TODO:return the size of all meshes in a tuple 
 function Base.size(obj::MeshProduct)
-    return 1
+    return length.(obj.mlist)
 end
 
 function rank(obj::MeshProduct)
-    return length(mlist)
+    return length(obj.mlist)
 end
 
-#TODO: find the linearindex I corresponding to given index
+#TODO: find the linearindex I corresponding to the given index
 function index_to_linear(obj::MeshProduct, index...)
-    return 1
+    bn = reverse(Tuple(prod(size(obj)[1:n-1]) for (n,sz) in enumerate(size(obj))))
+    li = 1
+    for (i,id) in enumerate(index)
+      li = li+(id-1)*bn[i]
+    end
+    return li
 end
 
 #TODO: find the index corresponding to linearindex I
-function linear_to_index(obj::MeshProduct)
-    return 1
+function linear_to_index(obj::MeshProduct, I::Int)
+    d =rank(obj)
+    bn = reverse(Tuple(prod(size(obj)[1:n-1]) for (n,sz) in enumerate(size(obj))))
+    index = zeros(Int32,d)
+    index[1] = (I-1) ÷ bn[1] +1
+    for k in 2:d
+        index[k]=((I-1)%bn[k-1])÷bn[k]+1 
+    end
+    return Tuple(index)
 end
 
 
 
 #TODO:for all n meshes in mlist, return [..., (mlist[i])[index[i]], ...] 
 function Base.getindex(obj::MeshProduct, index...)
-    return 1
+    return Tuple(obj.mlist[i][id] for (i,id) in enumerate(index))
 end
 #TODO:find the index corresponds to linearindex I, then for all n meshes in mlist, return [..., (mlist[i])[index[i]], ...] 
 function Base.getindex(obj::MeshProduct, I::Int)
-    return 1
+    return getindex(meshprod,linear_to_index(meshprod,I)[1])
 end
 
 #TODO:return the sliced pieces of 
--- test/test_MeshProduct.jl
+++ test/test_MeshProduct.jl
@@ -1,7 +1,8 @@
 @testset "GreenFunc" begin
     @testset "MeshProduct" begin
-        mesh = [0.0, 1.0]
-        meshprod = MeshProduct(mesh,mesh)
+        mesh1 = [0.0, 1.0]
+        mesh2 = [0.0,1.0,2.0]
+        meshprod = MeshProduct(mesh1,mesh2)
         println(typeof(meshprod))
         println("size of mesh is $(size(mesh)), size of meshprod is $(size(meshprod))")
         println("length of mesh is $(size(mesh)), length of meshprod is $(size(meshprod))")
