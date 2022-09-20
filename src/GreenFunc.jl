c2a2d2fe239929af968ebed3361ad7142f209f5c
Xiansheng Cai <iintsjds@gmail.com>
2022-09-20 16:45:22 -0400

    add GreenSym into GreenFunc.jl so that legacy functions instant & dynamic are available. now tests should pass

--- src/GreenFunc.jl
+++ src/GreenFunc.jl
@@ -5,9 +5,14 @@ using StaticArrays, Lehmann, CompositeGrids
 
 include("green/Green.jl")
 export TimeDomain, ImTime, ReTime, ImFreq, ReFreq, DLRFreq
-export Green2DLR, toTau, toMatFreq, toDLR, dynamic, instant
+export Green2DLR, toTau, toMatFreq, toDLR
+
 include("green/GreenDLR.jl")
 export GreenDLR
+
+include("green/GreenSym.jl")
+export GreenSym2DLR, dynamic, instant
+
 include("green/MeshProduct.jl")
 export MeshProduct
 end
