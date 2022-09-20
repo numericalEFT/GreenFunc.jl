5d73847bca756b4604af288c54dcb0633f48e519
Kun Chen <chenkun0228@gmail.com>
2022-09-20 13:40:31 -0400

    refactor MeshProduct

--- src/green/MeshProduct.jl
+++ src/green/MeshProduct.jl
@@ -7,9 +7,8 @@ Cartisian product of 1 dimensional meshes
 #Members:
 """
 struct MeshProduct{MT}
-    mlist::MT
-    function MeshProduct(vargs...
-        )
+    meshes::MT
+    function MeshProduct(vargs...)
         #@assert all(v -> (v isa Mesh), vargs) "all arguments should variables"
         mprod = Tuple(v for v in vargs)
         mnew = new{typeof(mprod)}(mprod)
@@ -19,48 +18,47 @@ end
 
 
 #TODO:return the sum of length of all meshes
-Base.length(obj::MeshProduct) = reduce(*, length(m) for m in obj.mlist)
+Base.length(obj::MeshProduct) = reduce(*, length(m) for m in obj.meshes)
 
-#TODO:return the size of Ith mesh in mlist 
-Base.size(obj::MeshProduct, I::Int) = length(obj.mlist[I])
+#TODO:return the size of Ith mesh in meshes 
+Base.size(obj::MeshProduct, I::Int) = length(obj.meshes[I])
 
 #TODO:return the size of all meshes in a tuple 
-Base.size(obj::MeshProduct)=Tuple(length.(obj.mlist))
+Base.size(obj::MeshProduct) = Tuple(length.(obj.meshes))
 
-rank(obj::MeshProduct) = length(obj.mlist)
+rank(obj::MeshProduct) = length(obj.meshes)
 
 #TODO: find the linearindex I corresponding to the given index
 function index_to_linear(obj::MeshProduct, index...)
-    bn = reverse(Tuple(prod(size(obj)[1:n-1]) for (n,sz) in enumerate(size(obj))))
+    bn = reverse(Tuple(prod(size(obj)[1:n-1]) for (n, sz) in enumerate(size(obj))))
     li = 1
-    for (i,id) in enumerate(index)
-      li = li+(id-1)*bn[i]
+    for (i, id) in enumerate(index)
+        li = li + (id - 1) * bn[i]
     end
     return li
 end
 
 #TODO: find the index corresponding to linearindex I
 function linear_to_index(obj::MeshProduct, I::Int)
-    d =rank(obj)
-    bn = reverse(Tuple(prod(size(obj)[1:n-1]) for (n,sz) in enumerate(size(obj))))
-    index = zeros(Int32,d)
-    index[1] = (I-1) ÷ bn[1] +1
+    d = rank(obj)
+    bn = reverse(Tuple(prod(size(obj)[1:n-1]) for (n, sz) in enumerate(size(obj))))
+    index = zeros(Int32, d)
+    index[1] = (I - 1) ÷ bn[1] + 1
     for k in 2:d
-        index[k]=((I-1)%bn[k-1])÷bn[k]+1 
+        index[k] = ((I - 1) % bn[k-1]) ÷ bn[k] + 1
     end
     return Tuple(index)
 end
 
 
 
-#TODO:for all n meshes in mlist, return [..., (mlist[i])[index[i]], ...] 
-Base.getindex(obj::MeshProduct, index...) = Tuple(obj.mlist[i][id] for (i,id) in enumerate(index))
+#TODO:for all n meshes in meshes, return [..., (meshes[i])[index[i]], ...] 
+Base.getindex(obj::MeshProduct, index...) = Tuple(obj.meshes[i][id] for (i, id) in enumerate(index))
 
-#TODO:find the index corresponds to linearindex I, then for all n meshes in mlist, return [..., (mlist[i])[index[i]], ...] 
-function Base.getindex(obj::MeshProduct, I::Int)
-    index =linear_to_index(obj,I)
-    return Tuple(obj.mlist[i][id] for (i,id) in enumerate(index))
-end
+#TODO:find the index corresponds to linearindex I, then for all n meshes in meshes, return [..., (meshes[i])[index[i]], ...] 
+Base.getindex(obj::MeshProduct, I::Int) = Base.getindex(obj, linear_to_index(obj, I)...)
+# return Tuple(obj.meshes[i][id] for (i, id) in enumerate(index))
+# return Base.getindex(obj.meshes, I)
 
 #TODO:return the sliced pieces of 
 # function Base.view(obj::MeshProduct,inds...)
@@ -70,35 +68,36 @@ end
 # t[1] --> view of the first mesh
 
 
+# Check https://docs.julialang.org/en/v1/manual/interfaces/ for details on how to implement the following functions:
 Base.firstindex(obj::MeshProduct) = 1
 Base.lastindex(obj::MeshProduct) = length(obj)
 # iterator
-Base.iterate(obj::MeshProduct) = (obj[1],1)
-Base.iterate(obj::MeshProduct, state) = (state>=length(obj)) ? nothing : (obj[state+1],state+1)
+Base.iterate(obj::MeshProduct) = (obj[1], 1)
+Base.iterate(obj::MeshProduct, state) = (state >= length(obj)) ? nothing : (obj[state+1], state + 1)
 
 
 #TODO:nice print
-Base.show(obj::MeshProduct)=string("MeshProduct of:\n",join((repr(x) for x in obj.mlist),",\n"))
+Base.show(io::IO, obj::MeshProduct) = print(io, "MeshProduct of: $(obj.meshes)")
 
 
 """
- All meshes in mlist should have locate and volume functions. Here in meshproduct we just delegate these functions to the meshes, and return the proper array of returned values.
+ All meshes in meshes should have locate and volume functions. Here in meshproduct we just delegate these functions to the meshes, and return the proper array of returned values.
 """
 function locate(obj::MeshProduct, index...)
     return Tuple(locate(obj, index[mi]) for (mi, m) in enumerate(obj))
 end
 
 function volume(obj::MeshProduct, index...)
-     return reduce(*, volume(obj, index[mi]) for (mi, m) in enumerate(obj))
- end
+    return reduce(*, volume(obj, index[mi]) for (mi, m) in enumerate(obj))
+end
 
 function locate(obj::MeshProduct, I::Int)
-    index = linear_to_index(obj,I)
+    index = linear_to_index(obj, I)
     return (locate(obj, index[mi]) for (mi, m) in enumerate(obj))
 end
 
 function volume(obj::MeshProduct, I::Int)
-    index = linear_to_index(obj,I)
+    index = linear_to_index(obj, I)
     return reduce(*, volume(obj, index[mi]) for (mi, m) in enumerate(obj))
 end
 
--- test/test_MeshProduct.jl
+++ test/test_MeshProduct.jl
@@ -1,22 +1,43 @@
 @testset "GreenFunc" begin
     @testset "MeshProduct" begin
-        export MeshProduct
-        mesh1 = SimpleGrid.Uniform{Float64}([0.0, 1.0], 5)
-        mesh2 = SimpleGrid.Uniform{Float64}([0.0, 1.0], 5)
+
+        N1, N2 = 5, 7
+        mesh1 = SimpleGrid.Uniform{Float64}([0.0, 1.0], N1)
+        mesh2 = SimpleGrid.Uniform{Float64}([0.0, 1.0], N2)
         x = 3
         y = 4
         I = 14
-        meshprod = MeshProduct(mesh1,mesh2)
-        println(typeof(meshprod))
-        println("rank of meshprod is $(GreenFunc.rank(meshprod))")
-        println("size of mesh1 is $(size(meshprod,1)), size of mesh2 is $(size(meshprod,2))")
-        println("size of meshprod is $(size(meshprod)), length of meshprod is $(length(meshprod))")
-        println("test index_to_linear for index(($x),($y)):$(GreenFunc.index_to_linear(meshprod,x,y))")
-        println("test linear_to_index for I=$(I):$(GreenFunc.index_to_linear(meshprod,I))") 
-        println("test getindex with index input:\n",meshprod[x,y])
-        println("test getindex with linearindex input:\n",meshprod[I])
-        println("Print test:\n",show(meshprod))
+        meshprod = MeshProduct(mesh1, mesh2)
 
-    end   
+        println("test meshprod: ", meshprod)
+
+        @test meshprod isa MeshProduct{Tuple{typeof(mesh1),typeof(mesh2)}}
+        @test GreenFunc.rank(meshprod) == 2
+        @test size(meshprod, 1) == N1
+        @test size(meshprod, 2) == N2
+        @test size(meshprod) == (N1, N2)
+
+        # @inferred meshprod[1]
+        # @inferred meshprod[2]
+
+        function test_linear_index(mp, x, y, i)
+            @test GreenFunc.index_to_linear(meshprod, x, y) == i
+            @test GreenFunc.linear_to_index(meshprod, i) == (3, 4)
+            @test mp[x, y] == mp[i]
+            println(mp[i])
+        end
+
+        test_linear_index(meshprod, x, y, I)
+
+        # println(typeof(meshprod))
+        # println("rank of meshprod is $(GreenFunc.rank(meshprod))")
+        # println("size of mesh1 is $(size(meshprod,1)), size of mesh2 is $(size(meshprod,2))")
+        # println("size of meshprod is $(size(meshprod)), length of meshprod is $(length(meshprod))")
+        # println("test index_to_linear for index(($x),($y)):$(GreenFunc.index_to_linear(meshprod,x,y))")
+        # println("test linear_to_index for I=$(I):$(GreenFunc.index_to_linear(meshprod,I))")
+        # println("test getindex with index input:\n", meshprod[x, y])
+        # println("test getindex with linearindex input:\n", meshprod[I])
+        # println("Print test:\n", show(meshprod))
+    end
 end
 
