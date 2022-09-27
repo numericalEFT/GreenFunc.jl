a044e6a1874f1c74f4d5a16a072849fb8cf2519f
Xiansheng Cai <iintsjds@gmail.com>
2022-09-26 23:25:00 -0400

    add example/hubbard_response/ and two files to work on

---
+++ example/hubbard_response/HubbardRPA.jl
@@ -0,0 +1,4 @@
+module HubbardRPA
+#TODO: load rpa kernel of hubbard model from triqs and triqs_tprf
+
+end
---
+++ example/hubbard_response/sc_response_mc.jl
@@ -0,0 +1,4 @@
+include("./HubbardRPA.jl")
+using HubbardRPA.jl
+
+# solve linear response function and compute Tc 
