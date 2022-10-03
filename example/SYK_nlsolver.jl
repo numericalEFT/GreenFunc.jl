48639d277cd7bfb36ea400ef365cb346d6f952de
Kun Chen <chenkun0228@gmail.com>
2022-10-03 16:00:40 -0400

    add SYK_nlsolver.jl

--- example/SYK_nlsolver.jl
+++ example/SYK_nlsolver.jl
@@ -13,15 +13,15 @@ distance(a, b) = norm(a - b, 2) # return the 1-norm distance between a and b
 conformal_tau(τ, β) = π^(1 / 4) / sqrt(2β) * 1 / sqrt(sin(π * τ / β))
 reverseview(x) = view(x, reverse(axes(x, 1))) # reversed view of x
 
-const mesh_dlr = DLRFreq(β, isFermi; Euv=5.0, rtol=rtol, sym=:ph, rebuild=false)   # Initialize DLR grid
+const dlrmesh = DLRFreq(β, isFermi; Euv=5.0, rtol=rtol, sym=:ph, rebuild=false)   # Initialize DLR grid
 
 function selfenergy(Gt)
     ######### calculate sigma ###############
-    minus_tau = reverse(mesh_dlr.β .- Gt.mesh[1]) # Reversed imaginary time mesh point
-    Gt_inv = dlr_to_imtime(to_dlr(Gt, mesh_dlr), minus_tau) # G at beta - tau
+    minus_tau = reverse(β .- Gt.mesh[1]) # Reversed imaginary time mesh point
+    Gt_inv = dlr_to_imtime(to_dlr(Gt, dlrmesh), minus_tau) # G at beta - tau
     Gmt = reverseview(Gt_inv)
     Σt = J .^ 2 .* Gt .^ 2 .* Gmt  # SYK self-energy in imaginary time
-    return dlr_to_imfreq(to_dlr(Σt, mesh_dlr))
+    return dlr_to_imfreq(to_dlr(Σt, dlrmesh))
 end
 
 function dyson(Gt)
@@ -31,26 +31,23 @@ function dyson(Gt)
     Gω = 1im * imag.(-1 ./ (freq .+ Σω))
 
     #############  Gω --> Gτ  ###############
-    return dlr_to_imtime(to_dlr(Gω, mesh_dlr))
+    return dlr_to_imtime(to_dlr(Gω, dlrmesh))
 end
 
 function nlsolve(G_t, tol=rtol, maxiter=1000, verbose=false, mix=0.1)
     for iter in 1:maxiter
         G_t_new = dyson(G_t)
-        if verbose
-            if iter % (maxiter / 10) == 0
-                println("round $iter: change $(diff(G_t_new, G_t)), distance $(distance(G_t_new, G_t))")
-            end
+        if verbose && (iter % (maxiter / 10) == 0)
+            println("round $iter: change $(diff(G_t_new, G_t)), distance $(distance(G_t_new, G_t))")
         end
         if maximum(abs.(G_t_new - G_t)) < tol && iter > 10
             return G_t_new
         end
-
         G_t = mix .* G_t_new + (1 - mix) .* G_t # Linear mixing
     end
 end
 
-const mesh = ImTime(β, isFermi; Euv=5.0, grid=mesh_dlr.dlr.τ)
+const mesh = ImTime(β, isFermi; Euv=5.0, grid=dlrmesh.dlr.τ)
 const G_t = MeshArray(mesh; dtype=ComplexF64)
 fill!(G_t, 0.0)
 G = nlsolve(G_t)
